0c7da3ad5e4a01e2264d4b9dba4a611bf0892e61
airotivmaria <mariavitoria6019@gmail.com>
2024-09-10 09:33:43 -0300

    primeiros comandos

---
+++ index.js
@@ -0,0 +1,16 @@
+// arrays, objetos 
+let meta = {
+    value: 'ler um livro por mês',
+    checked: false,
+    isChecked: (info) => {
+        console.log(info)
+    }
+}
+
+console.log(meta.value)
+meta.isChecked()
+
+//function // arrow function
+let criarMeta = () => {}
+
+// function criarMeta() {}
