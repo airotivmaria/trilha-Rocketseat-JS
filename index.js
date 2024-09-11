fba7a3ae84a553639c73fb5e3f5d9239c6ff946c
airotivmaria <mariavitoria6019@gmail.com>
2024-09-11 09:03:21 -0300

    problema nas marcadas

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
-const { select } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
+
+let metas = []
+
+const cadastrarMeta = async () => {
+    const meta = await input({message: "Digite a meta: "})
+
+    if (meta.length == 0) {
+        console.log("A meta não pode ser vazia.")
+        return
+    }
+
+    metas.push({value: meta, checked: false})
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o enter para finalizar a etapa.",
+        choices: [...metas],
+        instructions: false,
+    })
+    
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    if (respostas.length == 0){
+        console.log("Nenhuma meta selecionada.")
+        return
+    }
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+        meta.checked = true
+    })
+
+    console.log("Meta(s) marcadas como concluídas!")
+}
+
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+
+    if(realizadas.length == 0) {
+        console.log("Não existem metas realizadas.")
+        return
+    }
+
+    await select({
+        message: "Metas Realizadas",
+        choices: [...realizadas]
+    })
+}
+
 
 
 const start = async () => {
@@ -17,6 +73,10 @@ const start = async () => {
                     value: "listar"
                 },
                 {
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -26,10 +86,14 @@ const start = async () => {
 
         switch(opcao){
             case "cadastrar":
-                console.log("Vamos Cadastrar")
+                await cadastrarMeta()
+                console.log(metas)
                 break
             case "listar":
-                console.log("Vamos listar")
+                await listarMetas()
+                break
+            case "realizadas":
+                await metasRealizadas()
                 break
             case "sair":
                 return
